7f19ca356445ce215b8458df084ff13f105749fe
Olexii <babiy0617@gmail.com>
2025-10-17 18:08:26 +0300

    Add models Task and Worker(AbstractUser)

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
 
@@ -15,3 +16,24 @@ class Position(models.Model):
         return self.name
 
 
+class Task(models.Model):
+    class StrChoices(models.TextChoices):
+        URGENT = "URG", "Urgent"
+        HIGH = "HI", "High"
+        MEDIUM = "MED", "Medium"
+        LOW = "LOW", "Low"
+    name =models.CharField(max_length=63)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=3,
+        choices=StrChoices.choices,
+        default=StrChoices.MEDIUM,
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField("Worker", related_name="assignees")
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.DO_NOTHING)
