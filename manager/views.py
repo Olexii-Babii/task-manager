901fc8e2d7eb24748333d86160c68a2ddd594218
Olexii <babiy0617@gmail.com>
2025-10-18 12:02:23 +0300

    Add and update urls.py in manager and def index in views
    Added new urls.py file in manager and created new path to home page.
    Also created prototype of index func in views

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Worker, Task
 
-# Create your views here.
+def index(request) -> HttpResponse:
+    num_workers = get_user_model().objects.count()
+    num_tasks = Task.objects.count()
+    return HttpResponse("<h1>Hello world<h1/>")
