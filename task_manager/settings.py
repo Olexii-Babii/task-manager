3c19da086b3bd2835d001ee9573e94b3953caf24
Olexii <babiy0617@gmail.com>
2025-10-19 13:54:26 +0300

    Add django debug toolbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "manager",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "task_manager.urls"
@@ -128,3 +130,6 @@ STATICFILES_DIRS = [
  BASE_DIR / "static",
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
